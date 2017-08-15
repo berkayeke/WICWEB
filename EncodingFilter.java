a6b269183a987726a9c3e79856be61f45bea8cae
ekeberkay <berkayeke@hotmail.com>
2017-08-15 23:12:09 +0300

    Servletler

--- EncodingFilter.java
+++ EncodingFilter.java
@@ -13,17 +13,13 @@ public class EncodingFilter implements Filter {
 
 	@Override
 	public void destroy() {
-
 	}
-
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
 			throws IOException, ServletException {
 		request.setCharacterEncoding("UTF-8");
 		filterChain.doFilter(request, response);
-	
 	}
-
 	@Override
 	public void init(FilterConfig arg0) throws ServletException {
 	}
